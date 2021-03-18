5035f5f8bcf20c7eec820fe6bac5e337706e881b
velykitty <jiyunee13@gmail.com>
2021-03-18 18:40:28 +0900

    create pages -route 컴포넌트로 특정 주소에 컴포넌트 연결 -link 컴포넌트 사용해 다른 주소로 이동 -route 하나에 여러 개의 path 설정

---
+++ src/About.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const About = () => {
+    return(
+        <div>
+            <h1>intro</h1>
+            <p>the objective of this project is to practice basic skills of the react router</p>
+        </div>
+    );
+};
+
+export default About;
---
+++ src/Home.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Home = () => {
+    return (
+        <div>
+            <h1>Home😁</h1>
+            <p>home, the first page people would encounter</p>
+        </div>
+    );
+};
+
+export default Home;
