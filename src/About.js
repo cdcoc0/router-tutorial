8d12cd8baf39ee4ccd346b23cee1fde21b2570a7
velykitty <jiyunee13@gmail.com>
2021-03-19 02:09:17 +0900

    URL query

--- src/About.js
+++ src/About.js
@@ -1,10 +1,17 @@
 import React from 'react';
+import qs from 'qs';
 
-const About = () => {
+const About = ({location}) => {
+    const query = qs.parse(location.search, {
+        ignoreQueryPrefix: true
+    });
+    const showDetail = query.detail === 'true';
+
     return(
         <div>
             <h1>intro</h1>
             <p>the objective of this project is to practice basic skills of the react router</p>
+            {showDetail && <p>set detail true</p>}
         </div>
     );
 };
